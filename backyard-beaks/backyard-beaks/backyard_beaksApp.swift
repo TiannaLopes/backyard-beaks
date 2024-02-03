5dcd17c04a88a2e97a3f7b563e5c7ea8e6569df4
TiannaLopes <tiannaalinalopes@gmail.com>
2024-02-03 14:06:30 +0200

    Initialize app

---
+++ backyard-beaks/backyard-beaks/backyard_beaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  backyard_beaksApp.swift
+//  backyard-beaks
+//
+//  Created by Tianna Alina Lopes on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct backyard_beaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
